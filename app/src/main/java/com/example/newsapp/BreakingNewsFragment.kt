1623e025907c6e5d76f2035dbc2b5fcb3a5ac6e5
SK Nur Ahmad <sknurahmad2@gmail.com>
2024-01-07 23:20:32 +0530

    Fragment navigation sync with bottom navigation

---
+++ app/src/main/java/com/example/newsapp/BreakingNewsFragment.kt
@@ -0,0 +1,11 @@
+package com.example.newsapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
+
+}
--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.btm_navigation)
+        val navHostFragment = findNavController(R.id.home_fragment)
+
+        bottomNavigation.setupWithNavController(navHostFragment)
     }
 }
